ce3eebd2dffb87cc7713055efde45a2854888935
Chuka nnoli <143997599+Chukannoli@users.noreply.github.com>
2024-05-20 17:09:13 +0200

    creating app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      
+      <div className="main-container">
+        <h1>Budget App</h1>
+        <div className="budget-info">
+          <div className="budget-container">
+            
+          </div>
+          <div className="remaining-container">
+            
+          </div>
+          <div className="expense-total">
+            
+          </div>
+        </div>
+
+        <div className="expense-form">
+          <h3>Add Expense</h3>
+          
+        </div>
+
+        <div className="expense-list-container">
+          <h3>Expenses</h3>
+          
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
