09aa2d651f9965eff5779f237b0462aad5c4bc99
Chuka nnoli <143997599+Chukannoli@users.noreply.github.com>
2024-05-20 21:16:57 +0200

    Importing budget component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import "./App.css";
+import { Budget } from "./components/Budget/Budget";
 import { AppProvider } from "./context/AppContext";
 
 function App() {
   return (
     <>
-    <AppProvider>
-    <div className="main-container">
-        <h1>Budget App</h1>
-        <div className="budget-info">
-          <div className="budget-container">
-            
+      <AppProvider>
+        <div className="main-container">
+          <h1>Budget App</h1>
+          <div className="budget-info">
+            <div className="budget-container">
+              <Budget />
+            </div>
+            <div className="budget-stats">
+              <div className="remaining-container">
+                
+              </div>
+              <div className="expense-total">
+
+              </div>
+
+            </div>
           </div>
-          <div className="remaining-container">
-            
+
+          <div className="expense-form">
+            <h3>Add Expense</h3>
+
           </div>
-          <div className="expense-total">
-            
+
+          <div className="expense-list-container">
+            <h3>Expenses</h3>
+
           </div>
         </div>
-
-        <div className="expense-form">
-          <h3>Add Expense</h3>
-
-        </div>
-
-        <div className="expense-list-container">
-          <h3>Expenses</h3>
-          
-        </div>
-      </div>
-    </AppProvider>
+      </AppProvider>
 
     </>
   );
